cefd5c7e526833e0c2f7466430af3b21d5ad0fd2
DimitryAujour <106028495+DimitryAujour@users.noreply.github.com>
2024-03-12 20:02:45 -0400

    inital files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Quiz from './components/Quiz';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className="App">
+              <header className="App-header">
+                  <h1>Quizzical</h1>
+                  <p>Test your knowledge</p>
+                  <Link to="/Quiz"><button>Start Quiz</button></Link>
+              </header>
+
+              {/* Define Routes */}
+              <Routes>
+                  <Route path="/Quiz" element={<Quiz/>} />
+              </Routes>
+          </div>
+      </Router>
   );
 }
 
---
+++ src/components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Quiz() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Quizzical</h1>
+                <p>Show some questions here...</p>
+
+            </header>
+        </div>
+    );
+}
+export default Quiz;
